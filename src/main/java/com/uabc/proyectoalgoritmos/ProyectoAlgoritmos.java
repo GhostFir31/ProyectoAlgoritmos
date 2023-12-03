a1d40ae3dd212bd170971067c984607c1ac8ab42
Alan <alan.11.gt@gmail.com>
2023-12-02 21:07:59 -0800

    Prueba
    Contiene la presentación de nuestro proyecto

--- src/main/java/com/uabc/proyectoalgoritmos/ProyectoAlgoritmos.java
+++ src/main/java/com/uabc/proyectoalgoritmos/ProyectoAlgoritmos.java
@@ -11,6 +11,6 @@ package com.uabc.proyectoalgoritmos;
 public class ProyectoAlgoritmos {
 
     public static void main(String[] args) {
-        
+        System.out.println("Hola muy buenas a to2 wapisimos aki alan777 en un nuevo proyecto de github");
     }
 }
